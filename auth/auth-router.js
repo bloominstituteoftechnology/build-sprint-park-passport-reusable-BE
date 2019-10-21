9f04a8465ed2d37e187a16026a8f333756cb7d0a
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-21 13:22:48 -0400

    Set up users-model to include all the helper functions we'll need.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 // Models and Secrets go here
-
+const Users = require('../users/users-model');
+const secrets = require('../config/secrets');
 
 // Below endpoints begin with /api/auth
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash; 
+
+    
+})
