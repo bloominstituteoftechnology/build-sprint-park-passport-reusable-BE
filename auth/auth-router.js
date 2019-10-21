9fa7af1cf4af938fa31d331823e6de3bc023a4ee
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-21 13:28:02 -0400

    auth-router set up with login/register logic.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -12,5 +12,41 @@ router.post('/register', (req, res) => {
     const hash = bcrypt.hashSync(user.password, 10);
     user.password = hash; 
 
-    
-})
+    Users.add(user) 
+        .then(newUser => res.status(201).json(newUser))
+        .catch(err => res.status(500).json(err))
+});
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+
+            if (user && bcrypt.compareSync(password, user.password)) {
+                const token = generateToken(user);
+                res.status(200).json({ 
+                    message: `Hi, ${user.username}. Have a token...`, token
+                })
+            } else {
+                res.status(401).json({ message: 'Nope, wrong credentials.' })
+            }
+        })
+        .catch(err => res.status(500).json({ message: err }))
+});
+
+function generateToken(user) {
+    const payload = {
+        user: user.username,
+        subject: user.id,
+    };
+
+    const options = {
+        expiresIn: '1d'
+    };
+
+    return jwt.sign(payload, secrets.jwtSecret, options);
+}
+
+module.exports = router;
