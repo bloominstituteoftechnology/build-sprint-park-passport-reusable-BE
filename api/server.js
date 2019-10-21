8deb706ee24f2daa9d38637c1a6410615a9b3437
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-21 13:19:28 -0400

    Updated server to include auth route/router.

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 
 // Routers go here
-
+const authRouter = require('../auth/auth-router');
 
 const server = express();
 
@@ -12,6 +12,7 @@ server.use(express.json());
 server.use(cors());
 
 // Routes go here
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send("It's aliveeeee!")
