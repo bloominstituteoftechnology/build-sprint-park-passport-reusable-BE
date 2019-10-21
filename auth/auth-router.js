375557e011453ff6d98f55ab968cff86a626e525
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-21 13:16:45 -0400

    Created Secrets and Models file, which must be mentioned in auth-router.

---
+++ auth/auth-router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+// Models and Secrets go here
+
+
+// Below endpoints begin with /api/auth
