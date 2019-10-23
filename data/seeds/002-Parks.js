9a0751603a191de92ae97085179642b671e10fcd
Daniel Wallen <daniel.wallen1987@gmail.com>
2019-10-23 18:32:14 -0400

    Added seed file for Parks.

--- data/seeds/002-Parks.js
+++ data/seeds/002-Parks.js
@@ -1,13 +1,7 @@
-
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('parks').insert([
+    { name: 'Daytona Beach', description: 'Busiest beach you will ever see!' }, // 1
+    { name: 'Bays Mountain', description: 'Our wolf habitat is a huge hit.' } , // 2
+    { name: 'Joushua Tree',  description: 'Are you brave enough to climb this tree?' } // 3
+  ]);
 };
